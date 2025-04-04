38db1ecb7d300d00d163a3d290fd4e236d31ec91
Andrew Whelan <andrewhjwhelan@umail.ucc.ie>
2025-04-04 10:21:22 +0100

    Fixing the graph and tidying up comments a little

--- reflectance.m
+++ reflectance.m
@@ -13,7 +13,7 @@ Geometry setup: 2d wall of fixed length; fixed Tx; mobile Rx:
                                 \         /                               
                   +--------------\       /                                
                   |               \     /                                 
-                  | pi/2-angle_inc  \   /                                  
+                  | pi/2-angle_inc \   /                                  
                   |                 \ /                                   
       (0,0) -------------------- (refl_d,0) ------------------ (len_wall,0)
 %}
@@ -33,19 +33,19 @@ function T = spec_angle_tran(tx_d,tx_h,rx_d,rx_h,n1,n2)
     angle_i = spec_angle_inc(tx_d,tx_h,rx_d,rx_h);
     T = asin((n1*sin(angle_i))/n2);
 end
-function r = spec_refl_coeff(tx_d,tx_h,rx_d,rx_h,n1,n2,len_wall,perp)
+function R = spec_refl_coeff(tx_d,tx_h,rx_d,rx_h,n1,n2,len_wall,perp)
     % perp is a bool, deciding the EM-wave polarization relative to plane
-    % of incidence.
+    % of incidence. TODO: elaborate a little.
     refl_d = spec_refl_point(tx_d,tx_h,rx_d,rx_h);
     if refl_d > len_wall
-        r = 0; % since there won't be a real reflection point
+        R = 0; % since there won't be a real reflection point
     else
         angle_i = spec_angle_inc(tx_d,tx_h,rx_d,rx_h);
         angle_t = spec_angle_tran(tx_d,tx_h,rx_d,rx_h,n1,n2);
         if perp
-            r = -(sin(angle_i - angle_t))/(sin(angle_i + angle_t));
+            R = -(sin(angle_i - angle_t))/(sin(angle_i + angle_t));
         else
-            r = (tan(angle_i - angle_t))/(tan(angle_i + angle_t));
+            R = (tan(angle_i - angle_t))/(tan(angle_i + angle_t));
         end
     end
 end
@@ -59,11 +59,12 @@ end
 tx_d = 2;
 tx_h = 10;
 n1 = 1;
-n2 = 1.5; % e.g. plaster wall
-len_wall = 3*tx_d;
-perp=true; % polarization
+n2 = 1.5; % e.g. plaster wall. TODO: reference
+len_wall = 3*tx_d; %TODO: suggest sensible range of values
+perp=true; % polarization. TODO: elaborate
 
 steps = 200;
+%TODO: clean up subtitle and rethink scaling
 rx_init_d = 0;
 rx_final_d = 10*tx;
 rx_init_h = 0;
@@ -82,5 +83,4 @@ title('Spectral Reflectance Coefficient for Varying Rx Position')
 subtitle(['Tx dist: ' int2str(tx_d), ', Tx height: ' int2str(tx_h)]);
 xlabel('Rx distance')
 ylabel('Rx height')
-%TODO: clean up subtitle and rethink scaling
 %TODO: sensibility checks for graph
